98e5c5b414eaa4a7c6260fd9ac22a201f6c1eb03
Sung Je (Jay) Moon <moon11@seas.upenn.edu>
2024-05-02 07:53:53 -0400

    Updated front end design

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './HomePage.css';
+import logo from '../images/logo.jpg';
 
 const HomePage = () => {
   return (
@@ -13,6 +14,13 @@ const HomePage = () => {
         <Link to="/management">
           <button className="home-management-button">Card Management Page</button>
         </Link>
+      </div>
+      <div className="logo-container">
+        <img src={logo} alt="NBA Card Collector Logo" className="project-logo" />
+        <div className="project-description">
+          <h2>Card Court</h2>
+          <p>TheCardCourt is an innovative project developed by Luke Schalles, Jay Moon, Armaan Rathi, and Sean Chuang for CIS550. This project aims to leverage extensive NBA statistics, sourced from Kaggle and the NBA Advanced Statistics website, to create engaging applications that basketball fans and data enthusiasts will love.</p>
+        </div>
       </div>
       {/* <Link to="/dictionary"><button>Player Dictionary</button></Link> */}
     </div>
